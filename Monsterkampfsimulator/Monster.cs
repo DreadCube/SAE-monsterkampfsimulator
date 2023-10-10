8af466f09942bbbad54b9e2fadb0e38215e46506
Alexander Hofer <alexhofer@gmx.ch>
2023-10-10 13:35:39 +0200

    Refactor Monster class a little bit. Write a TakeDamage method for taking damage

--- Monsterkampfsimulator/Monster.cs
+++ Monsterkampfsimulator/Monster.cs
@@ -2,24 +2,24 @@
 {
     public class Monster
     {
-        // HP
+        // HP (Health Points)
         private float health;
 
-        // AP
+        // AP (Attack Points)
         private float attack;
 
-        // DP
+        // DP (Defense Points)
         private float defense;
 
-        // S
+        // SP (Speed Points)
         private float speed;
 
         // position of the monster
         private Vector2 position;
 
+
         // the healthbar instance of the monster
         private HealthBar healthBar;
-
 
         /**
 		 * The race has to defined from the child class
@@ -48,6 +48,31 @@
         }
 
         /**
+         * Applies damage to the monster (if damage is there)
+         */
+        private void TakeDamage(float damage)
+        {
+            health = Math.Max(0, health - damage);
+            healthBar.SetHealth(health);
+
+            /**
+             * If theres damage to the target monster we render the target monster
+             * red for a split of a time
+             */
+            if (damage > 0f)
+            {
+                Thread.Sleep(100);
+                Render(position, ConsoleColor.Red);
+                Thread.Sleep(100);
+            }
+        }
+
+        /**
+         * Getter for the defense
+         */ 
+        private float GetDefense() => defense;
+
+        /**
 		 * Every child clas of Monster (Ork, Troll, Goblin) has to define a image
 		 * that will be rendered
 		 */
@@ -73,6 +98,13 @@
          */
         public Vector2 GetPosition() => position;
 
+
+        /**
+         * Handles the attack logic.
+         * 1. Attacking Monster wll be forward animated to the target
+         * 2. The actual damage will be calculated and applied
+         * 3. The Attacking monster will be moved backwards
+         */
         public void Attack(Monster targetMonster)
         {
             int movingDirectionX = position.X - targetMonster.position.X < 0 ? 1 : -1;
@@ -92,25 +124,11 @@
                 }
             );
 
-            // TODO: frage an Supi.
-            // Eigener attack wert minusdefensiv wert des zu angreifendes Monsters i guess?
-            // Muss angreifendes Monster auch schaden bekommen?
-            float damage = Math.Max(0, attack - targetMonster.defense);
-            targetMonster.health -= damage;
-            targetMonster.health = Math.Max(0, targetMonster.health);
-            targetMonster.healthBar.SetHealth(targetMonster.health);
 
 
-            /**
-			 * If theres damage to the target monster we render the target monster
-			 * red for a split of a time
-			 */
-            if (damage > 0f)
-            {
-                Thread.Sleep(100);
-                targetMonster.Render(targetMonster.position, ConsoleColor.Red);
-                Thread.Sleep(100);
-            }
+            float damage = Math.Max(0, attack - targetMonster.GetDefense());
+            targetMonster.TakeDamage(damage);
+
 
             /**
 			 * Attack animation back from the target
